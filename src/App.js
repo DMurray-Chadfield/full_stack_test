89d74fbb8fc281406f02f723dc48da520ca51ef2
DMurray-Chadfield <dan.chadfield@gmail.com>
2022-01-12 11:40:26 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Display} from "./Display";
+import io from "socket.io-client";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: null};
+  }
+  componentDidMount() {
+    let socket = io("https://mst-full-stack-dev-test.herokuapp.com/");
+    socket.on("data-update", msg => {
+      this.setState({data: msg}); 
+    });
+    setInterval(() => {
+      console.log(this.state.data1);
+    }, 5000);
+  }
+  componentDidUpdate() {
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Playing next...</h1>
+          <Display data={this.state.data} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
